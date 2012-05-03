be86b51e485fdcafdec4e66628e44e01033fa94f
ceejaay <chad.jemmett@gmail.com>
2012-05-03 11:04:29 -0600

    found another bug

--- fave_number.rb
+++ fave_number.rb
@@ -1,9 +1,10 @@
 #completed 4/25/2012
 #ok, this had a bug. If you put in a string it tells you zero is a better number. So I came up with this workaround. It checks to see if the string is a number by testing to see if it's less than 9 if it is? it's a letter. b/c letters are less than numbers in text unicode whathave you. I'm proud of figuring this out.
+#ok. It has another bug. If you put in 9 it says it's not a number.
 # ask for the number
 puts "What is your favorite number?"
 faveNumber = gets.chomp
-  if faveNumber <= "9"
+  if faveNumber < "9"
   faveNumber = faveNumber.to_i
 new_number = faveNumber * rand(10)
 puts "That's a nice number but " + new_number.to_s + " is a much better number."
